0edb2535d73b401169d4dfeb2d9578ffab24ddc5
Steven Chia <stevenchia56@gmail.com>
2022-02-01 21:52:12 +0800

    set layout of streamlit app to be wide

--- visualise-housing-prices/streamlit/main2_no_functions.py
+++ visualise-housing-prices/streamlit/main2_no_functions.py
@@ -4,8 +4,7 @@ import pandas as pd
 import pandas_profiling
 from streamlit_pandas_profiling import st_profile_report
 
-# Import resale housing prices into a data frame
-# df = pd.read_csv("HDB resale flat prices (1990-1999).csv")
+st.set_page_config(layout="wide")
 
 st.title("Data Analyser")
 
