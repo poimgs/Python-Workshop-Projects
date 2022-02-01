43ce3dd9da4ffa69ed0f504c65c735a70433d341
Steven Chia <stevenchia56@gmail.com>
2022-02-01 21:25:53 +0800

    add new alternate project using pandas-profiling

---
+++ visualise-housing-prices/streamlit/main2_no_functions.py
@@ -0,0 +1,16 @@
+from distutils.command.upload import upload
+import streamlit as st
+import pandas as pd
+from streamlit_pandas_profiling import st_profile_report
+
+# Import resale housing prices into a data frame
+# df = pd.read_csv("HDB resale flat prices (1990-1999).csv")
+
+st.title("Data Analyser")
+
+uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
+
+if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+    pr = df.profile_report()
+    st_profile_report(pr)
