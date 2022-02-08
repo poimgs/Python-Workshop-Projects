fc145f34e2f99d51ecc11062d23183aec625ea17
Steven Chia <stevenchia56@gmail.com>
2022-02-08 18:51:46 +0800

    update qr code project a bit

--- qr-gnerator/main.py
+++ qr-gnerator/main.py
@@ -6,15 +6,18 @@ st.title("QR Code Generator")
 
 url = st.text_input("Enter the URL to be encoded in QR code")
 
-qr = qrcode.make(url)
-qr.save("qr.png")
+if url:
+    qr = qrcode.make(url)
+    qr.save("qr.png")
 
-st.image("qr.png", use_column_width=True)
+    st.image("qr.png", use_column_width=True)
 
-with open("qr.png", "rb") as f:
-    st.download_button(
-        data=f,
-        label="Download QR Code",
-        file_name="qr.png",
-        mime="image/png",
-    )
+    with open("qr.png", "rb") as f:
+        st.download_button(
+            data=f,
+            label="Download QR Code",
+            file_name="qr.png",
+            mime="image/png",
+        )
+else:
+    st.text("QR Code will be generated here!")
