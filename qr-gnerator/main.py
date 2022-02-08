f32b4fb7261e5bc9ad54ee9e33d69c561c4f9cc8
Steven Chia <stevenchia56@gmail.com>
2022-02-08 18:45:32 +0800

    test qr generator project

---
+++ qr-gnerator/main.py
@@ -0,0 +1,20 @@
+# Import libraries
+import streamlit as st
+import qrcode
+
+st.title("QR Code Generator")
+
+url = st.text_input("Enter the URL to be encoded in QR code")
+
+qr = qrcode.make(url)
+qr.save("qr.png")
+
+st.image("qr.png", use_column_width=True)
+
+with open("qr.png", "rb") as f:
+    st.download_button(
+        data=f,
+        label="Download QR Code",
+        file_name="qr.png",
+        mime="image/png",
+    )
