206e8e9b83bed13ad3a26989af72c0fa1fa19b33
Steven Chia <stevenchia56@gmail.com>
2022-02-25 03:56:44 +0800

    add more notes on resale prices automated analyser

--- resale-prices-automated-analyser/main.py
+++ resale-prices-automated-analyser/main.py
@@ -5,7 +5,9 @@ from streamlit_pandas_profiling import st_profile_report
 
 st.set_page_config(layout="wide")
 
-st.title("Resale  Analyser")
+st.title("Singapore HDB Resale Prices Automated Analyser")
+st.write("This app is designed to help you analyse the resale prices of HDB flats in Singapore.")
+st.caption("Source: https://data.gov.sg/dataset/resale-flat-prices?resource_id=adbbddd3-30e2-445f-a123-29bee150a6fe")
 
 df = pd.read_csv(
     "./resale-prices-automated-analyser/HDB resale flat prices (1990-1999).csv")
